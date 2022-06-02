66b24454f02898af9b40ecca0c6509dec17d8423
unknown <urceline0130@gmail.com>
2022-06-02 10:42:29 +0800

    Button is working, showing images already and player turn is alternating

--- Tic-Tac-Toe Version/Form1.Designer.cs
+++ Tic-Tac-Toe Version/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tic_Tac_Toe_Version
 {
-    partial class Form1
+    partial class TICTACTOE
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,223 @@ namespace Tic_Tac_Toe_Version
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TICTACTOE));
+            this.BT1 = new System.Windows.Forms.Button();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.BT3 = new System.Windows.Forms.Button();
+            this.BT4 = new System.Windows.Forms.Button();
+            this.BT5 = new System.Windows.Forms.Button();
+            this.BT6 = new System.Windows.Forms.Button();
+            this.BT7 = new System.Windows.Forms.Button();
+            this.BT8 = new System.Windows.Forms.Button();
+            this.BT9 = new System.Windows.Forms.Button();
+            this.EXITBUTTON = new System.Windows.Forms.Button();
+            this.TRYAGAINBUTTON = new System.Windows.Forms.Button();
+            this.PBXCAT = new System.Windows.Forms.PictureBox();
+            this.PBYDOG = new System.Windows.Forms.PictureBox();
+            this.CAT = new System.Windows.Forms.Label();
+            this.DOG = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBXCAT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBYDOG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BT1
+            // 
+            this.BT1.Location = new System.Drawing.Point(27, 52);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(131, 164);
+            this.BT1.TabIndex = 0;
+            this.BT1.Text = " ";
+            this.BT1.UseVisualStyleBackColor = true;
+            this.BT1.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // BT2
+            // 
+            this.BT2.Location = new System.Drawing.Point(164, 52);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(131, 164);
+            this.BT2.TabIndex = 1;
+            this.BT2.Text = " ";
+            this.BT2.UseVisualStyleBackColor = true;
+            this.BT2.Click += new System.EventHandler(this.BT2_Click);
+            // 
+            // BT3
+            // 
+            this.BT3.Location = new System.Drawing.Point(301, 52);
+            this.BT3.Name = "BT3";
+            this.BT3.Size = new System.Drawing.Size(131, 164);
+            this.BT3.TabIndex = 2;
+            this.BT3.Text = " ";
+            this.BT3.UseVisualStyleBackColor = true;
+            this.BT3.Click += new System.EventHandler(this.BT3_Click);
+            // 
+            // BT4
+            // 
+            this.BT4.Location = new System.Drawing.Point(27, 222);
+            this.BT4.Name = "BT4";
+            this.BT4.Size = new System.Drawing.Size(131, 164);
+            this.BT4.TabIndex = 3;
+            this.BT4.Text = " ";
+            this.BT4.UseVisualStyleBackColor = true;
+            this.BT4.Click += new System.EventHandler(this.BT4_Click);
+            // 
+            // BT5
+            // 
+            this.BT5.Location = new System.Drawing.Point(164, 222);
+            this.BT5.Name = "BT5";
+            this.BT5.Size = new System.Drawing.Size(131, 164);
+            this.BT5.TabIndex = 4;
+            this.BT5.Text = " ";
+            this.BT5.UseVisualStyleBackColor = true;
+            this.BT5.Click += new System.EventHandler(this.BT5_Click);
+            // 
+            // BT6
+            // 
+            this.BT6.Location = new System.Drawing.Point(301, 222);
+            this.BT6.Name = "BT6";
+            this.BT6.Size = new System.Drawing.Size(131, 164);
+            this.BT6.TabIndex = 5;
+            this.BT6.Text = " ";
+            this.BT6.UseVisualStyleBackColor = true;
+            this.BT6.Click += new System.EventHandler(this.BT6_Click);
+            // 
+            // BT7
+            // 
+            this.BT7.Location = new System.Drawing.Point(27, 392);
+            this.BT7.Name = "BT7";
+            this.BT7.Size = new System.Drawing.Size(131, 164);
+            this.BT7.TabIndex = 6;
+            this.BT7.Text = " ";
+            this.BT7.UseVisualStyleBackColor = true;
+            this.BT7.Click += new System.EventHandler(this.BT7_Click);
+            // 
+            // BT8
+            // 
+            this.BT8.Location = new System.Drawing.Point(164, 392);
+            this.BT8.Name = "BT8";
+            this.BT8.Size = new System.Drawing.Size(131, 164);
+            this.BT8.TabIndex = 7;
+            this.BT8.Text = " ";
+            this.BT8.UseVisualStyleBackColor = true;
+            this.BT8.Click += new System.EventHandler(this.BT8_Click);
+            // 
+            // BT9
+            // 
+            this.BT9.Location = new System.Drawing.Point(301, 392);
+            this.BT9.Name = "BT9";
+            this.BT9.Size = new System.Drawing.Size(131, 164);
+            this.BT9.TabIndex = 8;
+            this.BT9.Text = " ";
+            this.BT9.UseVisualStyleBackColor = true;
+            this.BT9.Click += new System.EventHandler(this.BT9_Click);
+            // 
+            // EXITBUTTON
+            // 
+            this.EXITBUTTON.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.EXITBUTTON.Location = new System.Drawing.Point(517, 450);
+            this.EXITBUTTON.Name = "EXITBUTTON";
+            this.EXITBUTTON.Size = new System.Drawing.Size(155, 52);
+            this.EXITBUTTON.TabIndex = 9;
+            this.EXITBUTTON.Text = "EXIT";
+            this.EXITBUTTON.UseVisualStyleBackColor = false;
+            // 
+            // TRYAGAINBUTTON
+            // 
+            this.TRYAGAINBUTTON.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.TRYAGAINBUTTON.Location = new System.Drawing.Point(517, 392);
+            this.TRYAGAINBUTTON.Name = "TRYAGAINBUTTON";
+            this.TRYAGAINBUTTON.Size = new System.Drawing.Size(155, 52);
+            this.TRYAGAINBUTTON.TabIndex = 10;
+            this.TRYAGAINBUTTON.Text = "TRY AGAIN";
+            this.TRYAGAINBUTTON.UseVisualStyleBackColor = false;
+            // 
+            // PBXCAT
+            // 
+            this.PBXCAT.Image = ((System.Drawing.Image)(resources.GetObject("PBXCAT.Image")));
+            this.PBXCAT.Location = new System.Drawing.Point(469, 198);
+            this.PBXCAT.Name = "PBXCAT";
+            this.PBXCAT.Size = new System.Drawing.Size(119, 146);
+            this.PBXCAT.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBXCAT.TabIndex = 11;
+            this.PBXCAT.TabStop = false;
+            // 
+            // PBYDOG
+            // 
+            this.PBYDOG.Image = ((System.Drawing.Image)(resources.GetObject("PBYDOG.Image")));
+            this.PBYDOG.Location = new System.Drawing.Point(594, 198);
+            this.PBYDOG.Name = "PBYDOG";
+            this.PBYDOG.Size = new System.Drawing.Size(119, 146);
+            this.PBYDOG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBYDOG.TabIndex = 12;
+            this.PBYDOG.TabStop = false;
+            // 
+            // CAT
+            // 
+            this.CAT.AutoSize = true;
+            this.CAT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.CAT.Location = new System.Drawing.Point(481, 179);
+            this.CAT.Name = "CAT";
+            this.CAT.Size = new System.Drawing.Size(58, 13);
+            this.CAT.TabIndex = 13;
+            this.CAT.Text = "PLAYER 1";
+            // 
+            // DOG
+            // 
+            this.DOG.AutoSize = true;
+            this.DOG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.DOG.Location = new System.Drawing.Point(607, 178);
+            this.DOG.Name = "DOG";
+            this.DOG.Size = new System.Drawing.Size(58, 13);
+            this.DOG.TabIndex = 14;
+            this.DOG.Text = "PLAYER 2";
+            // 
+            // TICTACTOE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(741, 586);
+            this.Controls.Add(this.DOG);
+            this.Controls.Add(this.CAT);
+            this.Controls.Add(this.PBYDOG);
+            this.Controls.Add(this.PBXCAT);
+            this.Controls.Add(this.TRYAGAINBUTTON);
+            this.Controls.Add(this.EXITBUTTON);
+            this.Controls.Add(this.BT9);
+            this.Controls.Add(this.BT8);
+            this.Controls.Add(this.BT7);
+            this.Controls.Add(this.BT6);
+            this.Controls.Add(this.BT5);
+            this.Controls.Add(this.BT4);
+            this.Controls.Add(this.BT3);
+            this.Controls.Add(this.BT2);
+            this.Controls.Add(this.BT1);
+            this.Name = "TICTACTOE";
+            this.Text = "TIC-TAC-TOE";
+            ((System.ComponentModel.ISupportInitialize)(this.PBXCAT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBYDOG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BT1;
+        private System.Windows.Forms.Button BT2;
+        private System.Windows.Forms.Button BT3;
+        private System.Windows.Forms.Button BT4;
+        private System.Windows.Forms.Button BT5;
+        private System.Windows.Forms.Button BT6;
+        private System.Windows.Forms.Button BT7;
+        private System.Windows.Forms.Button BT8;
+        private System.Windows.Forms.Button BT9;
+        private System.Windows.Forms.Button EXITBUTTON;
+        private System.Windows.Forms.Button TRYAGAINBUTTON;
+        private System.Windows.Forms.PictureBox PBXCAT;
+        private System.Windows.Forms.PictureBox PBYDOG;
+        private System.Windows.Forms.Label CAT;
+        private System.Windows.Forms.Label DOG;
     }
 }
 
--- Tic-Tac-Toe Version/Form1.cs
+++ Tic-Tac-Toe Version/Form1.cs
@@ -10,11 +10,132 @@ using System.Windows.Forms;
 
 namespace Tic_Tac_Toe_Version
 {
-    public partial class Form1 : Form
+    public partial class TICTACTOE : Form
     {
-        public Form1()
+        public TICTACTOE()
         {
             InitializeComponent();
+        }
+
+        Boolean PlayerTurn = false;
+        //Player1turn= ture;
+        //Player2turn=false;
+
+        private void BT1_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT1.Image = PBXCAT.Image; 
+            }
+            else 
+            {
+                BT1.Image = PBYDOG.Image; 
+            }
+            PlayerTurn = !PlayerTurn; 
+        }
+
+        private void BT2_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT2.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT2.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT3_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT3.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT3.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT4_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT4.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT4.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT5_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn ) 
+            {
+                BT5.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT5.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT6_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT6.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT6.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT7_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn) 
+            {
+                BT7.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT7.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT8_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn )
+            {
+                BT8.Image = PBXCAT.Image;
+            }
+            else 
+            {
+                BT8.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
+        }
+
+        private void BT9_Click(object sender, EventArgs e)
+        {
+            if (PlayerTurn)
+            {
+                BT9.Image = PBXCAT.Image;
+            }
+            else
+            {
+                BT9.Image = PBYDOG.Image;
+            }
+            PlayerTurn = !PlayerTurn;
         }
     }
 }
